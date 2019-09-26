927cd970e224f3afccb4588aff864eeb678e0ace
Ayushi <Ayushi@DESKTOP-OU0TTSG>
2019-09-26 18:26:22 +0530

    Spring form with validation

--- src/main/java/com/ssi/DetailsModel.java
+++ src/main/java/com/ssi/DetailsModel.java
@@ -3,14 +3,21 @@ package com.ssi;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
 
 @Entity
-@Table(name="employee")
+@Table(name = "employee")
 public class DetailsModel {
-    @Id
+	@Id
 	private int eno;
+	@Pattern(regexp = "[a-zA-Z]*", message = "Only Alphabates Allowed")
 	private String name;
+	@Pattern(regexp = "[a-zA-Z]*", message = "Only Alphabates Allowed")
 	private String bankName;
+	@Max(value = 100, message = "Maximum Score Can Be 10000000 Only")
+	@Min(value = 0, message = "Negative balance Is Not Allowed")
 	private int amount;
 
 	public int getEno() {
--- src/main/webapp/WEB-INF/views/insert.jsp
+++ src/main/webapp/WEB-INF/views/insert.jsp
@@ -6,10 +6,10 @@
 	<hr>
 	<form:form action="insertconfirm" modelAttribute="employee">
 		<pre>
-		Employee-no	<form:input path="eno"/>	
-		Employee-Name	<form:input path="name"/>	
-		Bank-Name	<form:input path="bankName"/>
-		Amount    <form:input path="amount"/>  
+		Employee-no	<form:input path="eno"/>  <form:errors path="eno"/>
+		Employee-Name	<form:input path="name"/>	 <form:errors path="name"/> 
+		Bank-Name	<form:input path="bankName"/>   <form:errors path="bankName"/>
+		Amount    <form:input path="amount"/>   <form:errors path="amount"/>
 			
 			<input type="submit" value="Save" />
 		</pre>
